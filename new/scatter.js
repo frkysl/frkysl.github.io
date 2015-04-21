0ecbca63f3608a6b2cbd3babf724a95c5acea1aa
frkysl <uysalfaruk@hotmail.com>
2015-04-21 15:24:40 -0500

    Update scatter.js

--- new/scatter.js
+++ new/scatter.js
@@ -1,7 +1,7 @@
 
 
 
-var margin = {top: 40, right: 40, bottom: 40, left: 40},
+var margin = {top: 50, right:50, bottom: 50, left: 50},
     w = 6*50 + margin.left - margin.right,
     h = 9*50 + margin.top - margin.bottom;
  
